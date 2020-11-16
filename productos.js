09d828326c15d086b786a7c219203734d9455f28
Nicolas Guelfo <guelfo26@gmail.com>
2020-11-16 14:27:34 -0300

    archivo con las funciones para productos

---
+++ productos.js
@@ -0,0 +1,54 @@
+const {Products} = require("./models");
+const {Sequelize} = require("sequelize");
+const sequelize = new Sequelize("delillah", "root", "", {
+    host: "127.0.0.1",
+    dialect: "mysql",
+});
+
+async function nuevoProducto(req,res){
+
+    await Products.findAll({
+
+        where: {
+            prod_name: req.body.prodname
+        }
+    })
+    .then(data => {
+        if(JSON.stringify(data) !== '[]'){
+            res.status(400).send("el producto ingresado ya existe, intente con otro");
+        } else {
+            await Products.create({
+                prod_name: req.body.prodname,
+                prod_price: req.body.prodprice,
+                prod_img: req.body.prodimg,
+                prod_isFav: 0
+            })
+            .then(result => {
+                res.status(201).json({msg: "El producto fue creado correctamente"});
+            })
+            .catch(err => {
+                res.status(400).send("Hubo un error en la creacion del producto. Intente mas tarde", err);
+            });
+        }
+    })
+    .catch(err => {
+        res.status(400).send("Ha habido un error en su consulta, intente nuevamente mas tarde", err);
+    });
+}
+
+async function misProductos(req,res){}
+
+async function productoPorId(req,res){}
+
+async function modificarProducto(req,res){}
+
+async function elminarProdcuto(req,res){}
+
+
+module.exports = {
+    nuevoProducto,
+    misProductos,
+    productoPorId,
+    modificarProducto,
+    elminarProdcuto
+}
